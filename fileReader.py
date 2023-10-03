92883f385463ff2925358a9e87a87078bda632d5
Collin Squires <squirrelly392@gmail.com>
2023-10-03 10:30:09 -0600

    added temple predictions

--- fileReader.py
+++ fileReader.py
@@ -16,3 +16,13 @@ for line in temple_dicts:
 area_dicts = []
 
 print(f"Imported {count} temples.")  # This line of code is just to make sure that each temple was imported from the CSV file.
+
+prediction_dicts = []
+
+with open("Predictions.csv", 'r') as p_file:
+    csv_prediction_reader = csv.DictReader(p_file)
+    for row in csv_prediction_reader:
+        prediction_dicts.append(row)
+
+for line in prediction_dicts:
+    print(f"displayPoint(graphicsLayer, '{line['Location']}', {line['Latitude']}, {line['Longitude']}, 'Prediction', 'Confidence Level: {line['Confidence Level']}');")
